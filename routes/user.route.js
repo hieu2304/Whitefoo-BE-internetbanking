8b1656b43aa84e69ebe1f967569bef4c09326bd4
NgocTheNguyen <nguyenthengoc1998@live.com>
2020-06-24 21:53:55 +0700

    fixed routes+controllers gone wrong

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const controller = require('../controllers/user.controller');
 
 router.post('/forgotpassword', controller.postForgotPassword);
-router.post('/verifyforgotcode', controller.postForgotPassword);
-router.post('/updatenewpassword', controller.postForgotPassword);
+router.post('/verifyforgotcode', controller.postVerifyForgotCode);
+router.post('/updatenewpassword', controller.postUpdateNewPassword);
 
 module.exports = router;
