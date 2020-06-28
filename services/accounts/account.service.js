5732b0f78c54e5a324f56577d1439f0bd748658d
NgocTheNguyen <nguyenthengoc1998@live.com>
2020-06-28 14:11:29 +0700

    add account services

--- services/accounts/account.service.js
+++ services/accounts/account.service.js
@@ -1,8 +1,35 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 const Model = Sequelize.Model;
+const randomHelper = require('../../helpers/random.helper');
 
-class account extends Model {}
+class account extends Model {
+	static async checkIfExistAccountId(_id) {
+		const isExist = await account.findOne({
+			where: {
+				accountId: _id
+			}
+		});
+		if (isExist) return true;
+		return false;
+	}
+
+	static async getUniqueRandomAccountId() {
+		var randomAccountId = randomHelper.getRandomNumber(15);
+		while (await account.checkIfExistAccountId(randomAccountId)) {
+			randomAccountId = randomHelper.getRandomNumber(15);
+		}
+		return randomAccountId;
+	}
+
+	static async createNewAccount(request) {
+		newAccountId = await account.getUniqueRandomAccountId();
+
+		//nếu tài khoản thanh toán thì chỉ thêm bảng account
+
+		//nếu tài khoản tiết kiệm, phải gọi và thêm vào account_accumulated
+	}
+}
 
 account.init(
 	{
