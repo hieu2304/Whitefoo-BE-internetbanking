c661a5f4e8d3a4cfe66a5a0208a0339c58cd964e
hieu2304 <dangtrunghieu2304@gmail.com>
2020-06-30 14:21:33 +0700

    update lại 2 chức năng đổi mật khẩu

--- controllers/internal.controller.js
+++ controllers/internal.controller.js
@@ -1,10 +1,43 @@
 const accountService = require('../services/accounts/account.service');
 const asyncHandler = require('express-async-handler');
+const userService =require('../services/users/user.service');
 
+// create Account Internal post
 module.exports.postCreateAccount = asyncHandler(async function(req, res, next) {
 	return res.json({ message: 'success get into post Create account', postsList });
 });
 
+// create Account Internal get
 module.exports.getCreateAccount = function(req, res, next) {
 	return res.status(200).send({ message: 'OK' });
 };
+
+//Internal staff Search List User
+module.exports.searchListUser = asyncHandler(async function(req, res, next){
+	return res.status(200).send({ message: 'OK' });
+});
+
+//Internal staff Search User by request
+module.exports.searchUser = asyncHandler(async function(req, res, next){
+	return res.status(200).send({ message: 'OK' });
+});
+
+//Internal staff Search Account Bank
+module.exports.searchAccount = asyncHandler(async function(req, res, next){
+	return res.status(200).send({ message: 'OK' });
+});
+
+//Internal staff Search User by Id
+module.exports.searchUserbyId = asyncHandler(async function(req, res, next){
+	return res.status(200).send({ message: 'OK' });
+});
+
+//Internal staff Search User's bank account
+module.exports.searchUserAccount = asyncHandler(async function(req, res, next){
+	return res.status(200).send({ message: 'OK' });
+});
+
+//Internal staff Search account's history_payment
+module.exports.searchAccountHistory = asyncHandler(async function(req, res, next){
+	return res.status(200).send({ message: 'OK' });
+});
--- routes/internal.route.js
+++ routes/internal.route.js
@@ -6,7 +6,22 @@ const authMiddleware = require('../middlewares/auth.middleware');
 router.use(authMiddleware.loginRequired);
 
 //create new account for user
-router.get('/createaccount', controller.getCreateAccount);
-router.post('/createaccount', controller.postCreateAccount);
+router.get('/createaccount', controller.getCreateAccount);////tạo tài khoản nhân viên qua HTTP Get
+
+router.post('/createaccount', controller.postCreateAccount);//tạo tài khoản nhân viên qua HTTP Post
+
+//Internal staff Search User
+router.post('/searchlistuser',controller.searchListUser); //tìm kiếm danh sách các người dùng
+
+router.post('/searchuser',controller.searchUser);//tìm kiếm người dùng theo yêu cầu từ khóa
+
+router.post('/searchuseraccount',controller.searchUserAccount);//tìm kiếm tài khoản của người dùng
+
+router.post('/searchuserbyid',controller.searchUserbyId);//tìm kiếm người dùng bởi thông số id
+
+router.post('/searchaccount',controller.searchAccount);//tìm kiếm tài khoản ngân hàng
+
+router.post('searchaccounthistory',controller.searchAccountHistory);//tìm kiếm lịch sử giao dịch của tài khoản
 
 module.exports = router;
+
