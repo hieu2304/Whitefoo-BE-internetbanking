45c30a5aef791ff130ec54ce67add02346bc6372
NgocTheNguyen <nguyenthengoc1998@live.com>
2020-07-16 13:05:55 +0700

    add example for env

--- services/db.js
+++ services/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:postgres@127.0.0.1:5432/whitefoo';
+const connectionString = process.env.DATABASE_URL;
 const db = new Sequelize(connectionString);
 
 module.exports = db;
