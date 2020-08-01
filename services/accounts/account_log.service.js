226c838f1a2ab52f1a024e6c6bc857a12d241495
Hieu dang <dangtrunghieu2304@gmail.com>
2020-08-01 23:01:51 +0700

    pushaccount_Log

--- services/accounts/account_log.service.js
+++ services/accounts/account_log.service.js
@@ -6,7 +6,76 @@ const exchange_currencyService = require('../currency/exchange_currency.service'
 const errorListConstant = require('../../constants/errorsList.constant');
 const Decimal = require('decimal.js');
 
-class account_log extends Model {}
+class account_log extends Model {
+	static async pushAccountLog_transfer(accountIdA,accountIdB,balance,currency,msg,status){
+		var filterAction = 'transfer';
+		var action = accountIdA + ' chuyển khoản STK: ' + accountIdB;
+		if(status === 0){
+			filterAction = 'transfer fail';
+			action = accountIdA+' chuyển khoản STK: '+ accountIdB+' thất bại';
+		}
+		await account_log.pushAccountLog(accountIdA,accountIdB,balance,currency,msg,status,action, filterAction);
+	}
+
+	static async pushAccountLog_withdraw0(accountIdA,accountIdB,balance,currency,msg,status){
+		var filterAction = 'withdraw0';
+		var action = 'Rút tiền từ TK thanh toán: ' + accountIdA;
+		if(status === 0){
+			filterAction = 'withdraw0 fail';
+			action = 'Rút tiền từ TK thanh toán: ' + accountIdA +' thất bại';
+		}
+		await account_log.pushAccountLog(accountIdA,accountIdB,balance,currency,msg,status,action, filterAction);
+	}
+
+	static async pushAccountLog_withdraw1(accountIdA,accountIdB,balance,currency,msg,status){
+		var filterAction = 'withdraw1';
+		var action = 'Rút tiền từ TK tiết kiệm: ' + accountIdA;
+		if(status === 0){
+			filterAction = 'withdraw0 fail';
+			action = 'Rút tiền từ TK tiết kiệm: ' + accountIdA+' thất bại';
+		}
+		await account_log.pushAccountLog(accountIdA,accountIdB,balance,currency,msg,status,action, filterAction);
+	}
+
+	static async pushAccountLog_loadup(accountIdA,accountIdB,balance,currency,msg,status){
+		var filterAction = 'loadup';
+		var action = 'Nạp tiền ' + accountIdA;
+		if(status === 0){
+			filterAction ='loadup fail';
+			action ='Nạp tiền STK: '+ accountIdB +' thất bại';
+		}
+		await account_log.pushAccountLog(accountIdA,accountIdB,balance,currency,msg,status,action, filterAction);
+	}
+	static async pushAccountLog(accountIdA,accountIdB,balance,currency,msg,status,action, filterAction) {
+		const newDetail = {};//description
+		const theTimeTotal = new moment();
+		const newDate = moment(theTimeTotal).format('DD/MM/YYYY');
+		const newTime = moment(theTimeTotal).format('hh:mm:ss');
+
+		// xử lý description
+		newDetail.accountIdA = accountIdA;
+		newDetail.accountIdB = accountIdB;
+		newDetail.currencyType = currency;
+		newDetail.value = balance;
+		newDetail.status=status;
+		newDetail.date = newDate;
+		newDetail.time = newTime;
+		newDetail.message=msg;
+		newDetail.action = action;
+
+		await account_log.create({
+			accountIdA: accountIdA,
+			accountIdB: accountIdB,
+			status:status,
+			action: filterAction,
+			value:balance,
+			currencyType:currency,
+			message:msg,
+			description: JSON.stringify(newDetail),
+			time: theTimeTotal
+		});
+	}
+}
 
 account_log.init(
 	{
